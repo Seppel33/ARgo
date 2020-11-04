06d84751e1feb8ae95a7782cc4e1de47ed26d139
Sebastian Schmidt <sebastian6240@web.de>
2020-11-04 12:34:47 +0100

    Prefab Swich working

--- Assets/Scripts/PrefabSwitcher.cs
+++ Assets/Scripts/PrefabSwitcher.cs
@@ -15,11 +15,13 @@ namespace UnityEngine.XR.ARFoundation
         GameObject[] m_Prefabs = new GameObject[8];
         Dictionary<string, GameObject> m_InstatiatedPrefabs;
 
-        static string lastLog;
+        string lastImage = "";
 
         private void Awake()
         {
             m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
+            m_TrackedImageManager.requestedMaxNumberOfMovingImages = 1;
+            m_InstatiatedPrefabs = new Dictionary<string, GameObject>();
         }
 
         void OnEnable() => m_TrackedImageManager.trackedImagesChanged += OnChanged;
@@ -41,25 +43,49 @@ namespace UnityEngine.XR.ARFoundation
                 if( i <= m_Prefabs.Length)
                 {
                     GameObject prefab = m_Prefabs[i];
-                    GameObject scenePrefab = Instantiate(prefab, newImage.transform);
+                    m_InstatiatedPrefabs.Add(newImage.referenceImage.name ,Instantiate(prefab, newImage.transform));
+                    m_InstatiatedPrefabs[newImage.referenceImage.name].name = newImage.referenceImage.name;
+                    ChangePage(newImage);
                 }
 
-                D.LogNR("OnChanged.added " + newImage.referenceImage.name);
+                //D.LogNR("OnChanged.added " + newImage.referenceImage.name);
             }
 
             foreach (var updatedImage in eventArgs.updated)
             {
                 // Handle updated event
 
+                //if tracking an image (new or current)
+                if(updatedImage.trackingState == TrackingState.Tracking)
+                {
+                    ChangePage(updatedImage);
+                }
 
-
-                D.LogNR("OnChanged.updated " + updatedImage.referenceImage.name + "\n" + updatedImage.trackingState);
+                //D.LogNR("OnChanged.updated " + updatedImage.referenceImage.name + " " + updatedImage.trackingState);
             }
 
             foreach (var removedImage in eventArgs.removed)
             {
                 // Handle removed event
                 // never gets called on ARCore
+            }
+        }
+        void ChangePage(ARTrackedImage newPage)
+        {
+            
+            //if it is a new Page
+            if (!newPage.referenceImage.name.Equals(lastImage))
+            {
+                //deactivte old page prefab
+                if(lastImage.Length > 0)
+                {
+                    m_InstatiatedPrefabs[lastImage].SetActive(false);
+                }
+
+                //activete new page prefab
+                m_InstatiatedPrefabs[newPage.referenceImage.name].SetActive(true);
+
+                lastImage = newPage.referenceImage.name;
             }
         }
     }
