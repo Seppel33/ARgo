158b16826b13ecb63c3010573d8913143be45341
Inga <inga.mueller@stud.hshl.de>
2020-11-25 10:14:38 +0100

    Rotation um ein Objekt herum

---
+++ Assets/Scripts/RotateAround.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+
+    public GameObject rotTarget;
+    public int speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(rotTarget.transform.position, Vector3.up, speed * Time.deltaTime);
+    }
+}
