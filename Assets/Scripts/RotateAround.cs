e5a2efa87c050ec43bfdbd5d0b1125058bf04f8e
Sebastian Schmidt <sebastian6240@web.de>
2020-12-02 13:12:11 +0100

    Solar System Rework

--- Assets/Scripts/RotateAround.cs
+++ Assets/Scripts/RotateAround.cs
@@ -4,25 +4,71 @@ using UnityEngine;
 
 public class RotateAround : MonoBehaviour
 {
+    bool currentRealMode; //true = real Values
 
-    public GameObject rotTarget;
-    public float speed;
+    static bool useRealValues;
+    //public GameObject rotTarget;
+    public float speed; //angle per hour
 
-    public float selfRotate;
+    public float selfRotate; //angle per hour
 
-    public float distToSun;
+    public float distToSun; //mio km
+    public float scaledDistToSun;
+
+    public float planetScale; //mio km
+    public float scaledPlanetScale;
     void Start()
     {
-        
+        currentRealMode = false;
+        Setup();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (rotTarget != null)
+        transform.Rotate(Vector3.up, speed);
+        transform.GetChild(0).transform.Rotate(Vector3.up, selfRotate);
+
+        if(currentRealMode != SolarContentManager.ShowRealValue)
+        {
+            currentRealMode = SolarContentManager.ShowRealValue;
+            Setup();
+        }
+        /*if (rotTarget != null)
         {
             transform.RotateAround(rotTarget.transform.position, Vector3.up, (0.0005f * speed) * Time.deltaTime);
+        }*/
+
+    }
+
+    void Setup()
+    {
+        float position;
+        float scale;
+        if (currentRealMode)
+        {
+            position = distToSun;
+            scale = planetScale;
         }
-        transform.Rotate(new Vector3(0, 1, 0), (1 / selfRotate));
+        else
+        {
+            position = scaledDistToSun;
+            scale = scaledPlanetScale;
+        }
+        transform.GetChild(0).transform.localPosition =new Vector3(position,0,0);
+        if (name.Equals("Earth"))
+        {
+            transform.GetChild(1).transform.localPosition = new Vector3(position, 0, 0);
+        }
+
+        transform.GetChild(0).transform.localScale = new Vector3(scale, scale, scale);
+
+        //set lenght of trail
+        try
+        {
+            transform.GetChild(0).transform.GetComponentInChildren<TrailRenderer>().time = 0.209f/(position* Mathf.PI * (Mathf.Abs(speed)/180));
+        }
+        catch { }
+        
     }
 }
---
+++ Assets/Scripts/SolarContentManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarContentManager : MonoBehaviour
+{
+    public static bool ShowRealValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowRealValue = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeMode()
+    {
+        ShowRealValue = !ShowRealValue;
+    }
+}
