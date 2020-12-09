7dc379eda458dce01532622007b33e269182001e
badir <badir.al-sayyed@stud-hshl.de>
2020-12-09 13:34:03 +0100

    Sharing Funktionalität

---
+++ Assets/Scripts/Sharing.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Sharing : MonoBehaviour
+{
+    private string shareMessage;
+    public void ShareButton()
+    {
+        shareMessage = "Schau mal was ich mit den ARgonauten erlebt habe!";
+        StartCoroutine(TakeScreenshotAndShare());
+    }
+    
+    private IEnumerator TakeScreenshotAndShare()
+    {
+        yield return new WaitForEndOfFrame();
+
+        Texture2D ss = new Texture2D( Screen.width, Screen.height, TextureFormat.RGB24, false );
+        ss.ReadPixels( new Rect( 0, 0, Screen.width, Screen.height ), 0, 0 );
+        ss.Apply();
+
+        string filePath = Path.Combine( Application.temporaryCachePath, "shared img.png" );
+        File.WriteAllBytes( filePath, ss.EncodeToPNG() );
+
+        // To avoid memory leaks
+        Destroy( ss );
+
+        new NativeShare().AddFile( filePath )
+            .SetSubject("ARgo").SetText(shareMessage)
+            .SetCallback( ( result, shareTarget ) => Debug.Log( "Share result: " + result + ", selected app: " + shareTarget ) )
+            .Share();
+    }
+}
