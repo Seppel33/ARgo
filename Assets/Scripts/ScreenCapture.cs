190848d01bd34ea1ef0ca328d6e0419e30bdb8d3
badir <badir.al-sayyed@stud-hshl.de>
2020-12-09 12:59:16 +0100

    Funktionierende Screenshots

--- Assets/Scripts/ScreenCapture.cs
+++ Assets/Scripts/ScreenCapture.cs
@@ -40,6 +40,12 @@ public class ScreenCapture : MonoBehaviour
         arCamera.targetTexture = RenderTexture.GetTemporary(arCamera.pixelWidth, arCamera.pixelHeight, 16);
         takeScreenshotOnNextframe = true;
     }
+    
+    public void CaptureScreenshot() {
+        D.Log("Der Screenshot wurde gespeichert in: " + Application.persistentDataPath);
+        
+        UnityEngine.ScreenCapture.CaptureScreenshot("TestScreenshot.png");
+    }
 }
 
 
