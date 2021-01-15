ff13d60fc0b78ea0007f4f9d3d41b8f183557c71
badir <badir.al-sayyed@stud-hshl.de>
2021-01-15 17:38:13 +0100

    Working UI
    Settings Button, Audio Button and TaghideButton implementation

--- Assets/Scripts/Texts.cs
+++ Assets/Scripts/Texts.cs
@@ -15,12 +15,13 @@ public class Texts : MonoBehaviour
     private Text infobox;
     private int infobuttonCount;
     private GameObject[] infobuttons;
+    private GameObject[] arPrefabs;
+    
     
     void Start()
     {
         infobox = GameObject.FindWithTag("InfoText").GetComponent<Text>();
         
-        infobuttons = GameObject.FindGameObjectsWithTag("Bulb");
     }
 
     private void Update()
@@ -41,22 +42,48 @@ public class Texts : MonoBehaviour
         infobox.text = infoText[index];
     }
 
+    //In eigene Klasse
     public void TurnBulbOff(bool tog)
     {
+        infobuttons = GameObject.FindGameObjectsWithTag("Bulb");
         if (tog)
         {
             foreach(GameObject go in infobuttons)
             {
-                go.SetActive (false);
+                go.transform.GetChild(0).gameObject.SetActive(false);
             }
         }
         else
         {
             foreach(GameObject go in infobuttons)
             {
-                go.SetActive (true);
+                go.transform.GetChild(0).gameObject.SetActive(true);
             }
         }
-            
+    }
+
+    //In eigene Klasse
+    public void SoundOnOff(bool tog)
+    {
+        arPrefabs = GameObject.FindGameObjectsWithTag("ARPrefab");
+        if (tog)
+        {
+            foreach (GameObject go in arPrefabs)
+            {
+                go.GetComponent<AudioSource>().Pause();
+            }
+        }
+        else
+        {
+            foreach (GameObject go in arPrefabs)
+            {
+                go.GetComponent<AudioSource>().UnPause();
+            }
+        }
+    }
+
+    public void Tutorial(bool tog)
+    {
+        
     }
 }
