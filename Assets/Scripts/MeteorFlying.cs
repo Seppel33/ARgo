01f29a126f503fcebfa605845ac8f50ec4ceda45
Inga <inga.mueller@stud.hshl.de>
2021-01-20 14:04:01 +0100

    Prefab Scene MeteorField
    Die Argo wird mit Kartoffeln beschossen

--- Assets/Scripts/MeteorFlying.cs
+++ Assets/Scripts/MeteorFlying.cs
@@ -4,22 +4,39 @@ using UnityEngine;
 
 public class MeteorFlying : MonoBehaviour
 {
-    public GameObject meteor;
-    public Vector3 startpos;
-    public Vector3 lifetime;
+    Vector3 startpos;
+    Vector3 fightDirection;
 
+    int lifetime;
+    float meteorScale;
 
 
     void Start()
     {
-        startpos = new Vector3(0,0,0);
+        //Random Startopos innerhalb einer Plane wird zugewiesen
+        startpos = new Vector3(-7,Random.insideUnitSphere.y*2,Random.insideUnitSphere.z*2);
+        //Random Flugvektor
+        fightDirection = new Vector3(Random.Range(0.1f,1f),Random.Range(-0.05f,0.05f),Random.Range(-0.1f,0.1f));
+
+        lifetime = 30;
+
+
+        gameObject.transform.position = startpos;
+        meteorScale = Random.Range(0.01f,0.1f);
+
+        gameObject.transform.localScale = new Vector3(meteorScale,meteorScale,meteorScale);
+
         
-        
+
+
+
     }
 
     
     void Update()
     {
-        
+        gameObject.transform.Rotate(Random.Range(0.05f,0.4f),Random.Range(0.05f,0.4f),Random.Range(0.05f,0.4f),Space.Self);
+        gameObject.transform.Translate(fightDirection*Time.deltaTime,Space.World);
+        Destroy(gameObject, lifetime);
     }
 }
---
+++ Assets/Scripts/instatiateMeteors.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instatiateMeteors : MonoBehaviour
+{
+    public GameObject meteor;
+
+    void Start(){
+        StartCoroutine(startMeteors());
+    }
+    
+    
+    IEnumerator startMeteors()
+    {
+        while (gameObject.active){
+            GameObject.Instantiate(meteor);
+            yield return new WaitForSeconds(Random.Range(0.1f,2f));
+
+        }
+    }
+}
