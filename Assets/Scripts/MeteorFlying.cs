a398c08caf2bdffe19ba9ac515caf515694d2f61
Minkii <lea-sophie.timme@stud.hshl.de>
2021-01-20 12:06:06 +0100

    Anfang der Prefab Seiten

---
+++ Assets/Scripts/MeteorFlying.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorFlying : MonoBehaviour
+{
+    public GameObject meteor;
+    public Vector3 startpos;
+    public Vector3 lifetime;
+
+
+
+    void Start()
+    {
+        startpos = new Vector3(0,0,0);
+        
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
