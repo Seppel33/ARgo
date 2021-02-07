21994bbf458e533d1209c1976a39708f5f84dba0
badir <badir.al-sayyed@stud-hshl.de>
2021-02-07 16:31:40 +0100

    Landscape Mode done

--- Assets/Scripts/LandscapePortrait.cs
+++ Assets/Scripts/LandscapePortrait.cs
@@ -18,17 +18,19 @@ public class LandscapePortrait : MonoBehaviour
         if (Screen.orientation == ScreenOrientation.LandscapeLeft || Screen.orientation == ScreenOrientation.LandscapeRight)
         {
             infoBoxGameObject.transform.GetChild(0).gameObject.SetActive(false);
+            
             foreach (GameObject go in scaledUI)
             {
-                go.transform.localScale = new Vector3(0.5f, 0.5f, 1);
+                go.transform.localScale = new Vector3(0.35f, 0.35f, 1);
             }
-            scaledUI[0].transform.localScale = new Vector3(scaledUI[0].transform.localScale.x * scalefactor, scaledUI[0].transform.localScale.y * scalefactor, 1);
-            scaledUI[1].transform.localScale = new Vector3(scaledUI[1].transform.localScale.x * scalefactor * 1.1f, scaledUI[1].transform.localScale.y * scalefactor * 1.1f, 1);
+
         }
         else if (Screen.orientation == ScreenOrientation.Portrait)
         {
-            scaledUI[0].transform.localScale = new Vector3(0.7f, 0.7f, 1);
-            scaledUI[1].transform.localScale = new Vector3(1, 1, 1);
+            foreach (GameObject go in scaledUI)
+            {
+                go.transform.localScale = new Vector3(0.7f, 0.7f, 1);
+            }
         }
     }
 }
