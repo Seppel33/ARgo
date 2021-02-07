037849c6dfcba1b52bea7b28b3d3883c43933301
badir <badir.al-sayyed@stud-hshl.de>
2021-02-07 16:05:24 +0100

    Landscape mode

--- Assets/Scripts/LandscapePortrait.cs
+++ Assets/Scripts/LandscapePortrait.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class LandscapePortrait : MonoBehaviour
 {
     private GameObject infoBoxGameObject;
+    [SerializeField] private GameObject[] scaledUI;
+    [SerializeField] private float scalefactor;
 
     void Start()
     {
@@ -16,10 +18,17 @@ public class LandscapePortrait : MonoBehaviour
         if (Screen.orientation == ScreenOrientation.LandscapeLeft || Screen.orientation == ScreenOrientation.LandscapeRight)
         {
             infoBoxGameObject.transform.GetChild(0).gameObject.SetActive(false);
+            foreach (GameObject go in scaledUI)
+            {
+                go.transform.localScale = new Vector3(0.5f, 0.5f, 1);
+            }
+            scaledUI[0].transform.localScale = new Vector3(scaledUI[0].transform.localScale.x * scalefactor, scaledUI[0].transform.localScale.y * scalefactor, 1);
+            scaledUI[1].transform.localScale = new Vector3(scaledUI[1].transform.localScale.x * scalefactor * 1.1f, scaledUI[1].transform.localScale.y * scalefactor * 1.1f, 1);
         }
-        /*else if (Screen.orientation == ScreenOrientation.Portrait)
+        else if (Screen.orientation == ScreenOrientation.Portrait)
         {
-            infoBoxGameObject.transform.GetChild(0).gameObject.SetActive(true);
-        }*/
+            scaledUI[0].transform.localScale = new Vector3(0.7f, 0.7f, 1);
+            scaledUI[1].transform.localScale = new Vector3(1, 1, 1);
+        }
     }
 }
